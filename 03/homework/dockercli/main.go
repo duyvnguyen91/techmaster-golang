202079243f77d49fba4d2c47b0de8c0a61e6d65c
duynv13 <duynv13@onemount.com>
2021-09-16 18:58:49 +0700

    add toggle

---
+++ 03/homework/dockercli/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	term "github.com/nsf/termbox-go"
+	"log"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+
+	err := term.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer term.Close()
+
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Enter any key to see their ASCII code or press ESC button to quit")
+	fmt.Println("Press any key to see their ASCII code follow by Enter")
+
+	for i := 1; i <= 3; i++ {
+		ev := term.PollEvent()
+		if ev.Key == term.KeyF5 && i == 1 {
+			for _, container := range containers {
+				if container.State == "running" {
+					fmt.Println(container.ID, container.Names, container.Image, container.Ports, container.State)
+				}
+			}
+			fmt.Println("--------------------------")
+		}
+
+		if ev.Key == term.KeyF5 && i == 2 {
+			for _, container := range containers {
+				if container.State == "exited" {
+					fmt.Println(container.ID, container.Names, container.Image, container.Ports, container.State)
+				}
+			}
+			fmt.Println("--------------------------")
+		}
+
+		if ev.Key == term.KeyF5 && i == 3 {
+			i = 0
+			for _, container := range containers {
+				fmt.Println(container.ID, container.Names, container.Image, container.Ports, container.State)
+			}
+			fmt.Println("--------------------------")
+		}
+
+		if ev.Key == term.KeyEsc || ev.Key == term.KeyCtrlC {
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		}
+	}
+}
